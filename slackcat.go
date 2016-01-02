720f1a1009c803e3066aac07e0564aeeffcd5124
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-01-02 14:02:08 -0500

    bump version for release

--- slackcat.go
+++ slackcat.go
@@ -90,7 +90,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "slackcat"
 	app.Usage = "redirect a file to slack"
-	app.Version = "0.3"
+	app.Version = "0.4"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
 			Name:  "tee, t",
