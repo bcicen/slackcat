276a7e2129443bc2654170a593ebc9a7530a5577
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-01-10 15:00:21 -0500

    remove uneeded scopes from oauth request

--- slackcat.go
+++ slackcat.go
@@ -20,7 +20,7 @@ var version = "dev-build"
 const (
 	base_url  = "https://slack.com/oauth/authorize"
 	client_id = "7065709201.17699618306"
-	scope     = "channels%3Aread+groups%3Aread+im%3Aread+users%3Aread+chat%3Awrite%3Auser+files%3Awrite%3Auser+files%3Aread"
+	scope     = "channels%3Aread+groups%3Aread+im%3Aread+chat%3Awrite%3Auser+files%3Awrite%3Auser"
 )
 
 func getConfigPath() string {
